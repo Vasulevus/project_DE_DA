c348203e531bd42c51c35f19703d8c463e365d19
vasulevus <vasulevus@gmail.com>
2023-06-02 17:14:13 +0300

    Future plans + head query

--- import.py
+++ import.py
@@ -4,4 +4,10 @@ import numpy
 
 csv = pandas.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSKB0ufDrMisLON7WX023JBDTHnG0-ZXRJA6RLJSRV9iOnDA5IlHOYGavPwmpeIoenB3jhE-nxDRQth/pub?gid=2072565988&single=true&output=csv")
 df = pandas.DataFrame(csv)
-df
+df.head()
+#З'єднатися з sql server через розширення vscode. 
+#створити sql запити
+#під'єднатися через python до сервера
+#завантажити туди данні
+#зробити перевірочний запит
+#переробити все для airflow
