1d081176d33c9af98d59774326618242f4a59ee9
vasulevus <vasulevus@gmail.com>
2023-06-02 17:03:05 +0300

    Load data to dataframe

--- import.py
+++ import.py
@@ -1,1 +1,7 @@
 import pandas
+import numpy
+
+
+csv = pandas.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSKB0ufDrMisLON7WX023JBDTHnG0-ZXRJA6RLJSRV9iOnDA5IlHOYGavPwmpeIoenB3jhE-nxDRQth/pub?gid=2072565988&single=true&output=csv")
+df = pandas.DataFrame(csv)
+df
